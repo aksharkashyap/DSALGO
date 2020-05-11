ddc56999c981ffca2d8dc29c10bf618092ef6178
Akshar Kashyap <aksharcashyap@gmail.com>
2020-05-11 11:48:40 +0530

    c

--- GRAPH/TreeCentre.java
+++ GRAPH/TreeCentre.java
@@ -1,5 +1,7 @@
 package GRAPH;
 
+import java.util.Arrays;
+
 public class TreeCentre {
     TreeNode root;
      class TreeNode{
@@ -9,13 +11,16 @@ public class TreeCentre {
             this.val = val;
             child = null;
         }
-        TreeNode(){
-            child = null;
-        }
-
     }
 
-    void find_cetre(TreeNode root){
+    void find_degree(TreeNode root, int degree[]){
+        if(root == null) return;
+        System.out.print(root.val);
+        degree[root.val] = 1+root.child.length;
+        for(int i=0;i<root.child.length;i++)
+            find_degree(root.child[i], degree);
+    }
+    void find_cetre(TreeNode root, int[]degree){
 
     }
 
@@ -23,12 +28,12 @@ public class TreeCentre {
 
         TreeCentre t = new TreeCentre();
 
-        TreeNode a = t.new TreeNode(1);
-        TreeNode b = t.new TreeNode(2);
-        TreeNode c = t.new TreeNode(3);
-        TreeNode d = t.new TreeNode(4);
-        TreeNode e = t.new TreeNode(5);
-        TreeNode f = t.new TreeNode(6);
+        TreeNode a = t.new TreeNode(0);
+        TreeNode b = t.new TreeNode(1);
+        TreeNode c = t.new TreeNode(2);
+        TreeNode d = t.new TreeNode(3);
+        TreeNode e = t.new TreeNode(4);
+        TreeNode f = t.new TreeNode(5);
         t.root = a;
         a.child = new TreeNode[3];
         a.child[0] = b;
@@ -37,8 +42,10 @@ public class TreeCentre {
         c.child = new TreeNode[2];
         c.child[0] = e;
         c.child[1] = f;
-        
-        t.find_cetre(t.root);
-        
+        int V = 6;
+        int degree[] = new int[V];
+        t.find_cetre(t.root,degree);
+        t.find_degree(t.root, degree);
+        System.out.println(Arrays.toString(degree));
     }
 }
