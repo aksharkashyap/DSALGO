63a36d5971d101d9b964ce36ab6ab556412c1ee5
Akshar Kashyap <aksharcashyap@gmail.com>
2020-05-11 10:58:04 +0530

    t commit

---
+++ GRAPH/TreeCentre.java
@@ -0,0 +1,21 @@
+package GRAPH;
+
+public class TreeCentre {
+    TreeNode root;
+    class TreeNode{
+        TreeNode[] child;
+        int val;
+        TreeNode(int val){
+            this.val = val;
+            child = null;
+        }
+    }
+
+    void find_cetre(TreeNode root){
+
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
