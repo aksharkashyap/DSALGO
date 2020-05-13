b94c705143a89f7baa2ee8ef98eef1a43586eda2
Akshar Kashyap <aksharcashyap@gmail.com>
2020-05-13 16:41:27 +0530

    cmt

---
+++ BackTracking/RatInAMaze.java
@@ -0,0 +1,71 @@
+package BackTracking;
+//slight variation of knight tour
+/**A Maze is given as N*N binary matrix of blocks where source block is the upper left most block i.e., 
+ * maze[0][0] and destination block is lower rightmost block i.e., maze[N-1][N-1]. A rat starts from source 
+ * and has to reach the destination. The rat can move only in two directions: forward and down.
+In the maze matrix, 0 means the block is a dead end and 1 means the block can be used in the path 
+from source to destination. Note that this is a simple version of the typical Maze problem. 
+For example, a more complex version can be that the rat can move in 4 directions and a more complex 
+version can be with a limited number of moves. */
+
+public class RatInAMaze {
+
+    static boolean isSafePath(int mat[][],int x, int y,int N){
+        if(x<0 ||  x>=N || y<0 || y>=N || mat[x][y] !=1 )
+            return false;
+        return true;
+    }
+
+    static boolean solve(int[][]mat, int[][]visited , int x, int y, int N,int[]xMove,int[] yMove){
+        int next_x,next_y;
+        
+        if( x== N-1 && y == N-1 && mat[x][y]==1 ) return true;
+
+        for(int i=0;i<2;i++){
+                next_x = x + xMove[i];
+                next_y = y + yMove[i];
+            if(isSafePath(mat, next_x, next_y, N)){
+                visited[next_x][next_y] = 1;
+                if(solve(mat, visited, next_x, next_y, N, xMove, yMove))
+                    return true;
+                else visited[next_x][next_y] = 0;
+            }
+        }
+        return false;
+    }
+
+    static void rat_in_a_maze(int[][] mat, int srcX, int srcY, int destX, int destY, int N) {
+        int visited[][] = new int[N][N];
+        visited[srcX][srcY] = 1;
+        int xMove[] = {0,1}; 
+        int yMove[] = {1,0};
+        if(solve(mat,visited,srcX,srcY,N,xMove,yMove)){
+            System.out.println("Solution exist");
+            print_sol(visited, N);
+        }
+        else System.out.println("soln doesnt exist");
+    }
+
+    static void print_sol(int[][]visited,int N){
+        for(int i=0;i<N;i++){
+            for(int j=0;j<N;j++)
+                System.out.print(visited[i][j]+" ");
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        int mat[][] = {
+            {1, 0, 0, 0},
+            {1, 1, 0, 1},
+            {0, 1, 0, 0},
+            {1, 1, 1, 1}
+        };
+        int N = mat.length;
+        int srcX = 0, srcY = 0;
+        int destX = N-1, destY = N-1;
+
+        rat_in_a_maze(mat,srcX,srcY,destX,destY,N);
+    }
+}
+
+//application -> total number of paths from src to dst(with some constraint)
--- MatrixChainMultiplication/PalindromePartitioning.java
+++ MatrixChainMultiplication/PalindromePartitioning.java
@@ -1,5 +1,9 @@
 package MatrixChainMultiplication;
 
+import java.util.Arrays;
+
+import jdk.nashorn.internal.runtime.arrays.ArrayIndex;
+
 class PalindromePartitioning{
    
 
@@ -12,21 +16,34 @@ class PalindromePartitioning{
         return true;
     }
     
-    static int solve(char arr[], int i , int j){
+    static int solve(char arr[], int i , int j, int[][]memo){
         int ans = Integer.MAX_VALUE;
+        if(memo[i][j]!=-1) return memo[i][j];
         if(i>=j ||isPalindrome(arr,i,j)) return 0;
         
         for(int k=i;k<=j-1;k++){
-                int temp = solve(arr, i, k) + solve(arr, k+1, j) + 1;
+                int left,right;
+                if(memo[i][k]!= -1) left = memo[i][k];
+                else left = solve(arr, i, k,memo);
+
+                if(memo[k+1][j]!=-1) right = memo[k+1][j];
+                else right = solve(arr, k+1,j,memo);
+                
+                int temp = left + right + 1;
                 if(ans>temp) ans=temp;
         }
-        return ans;
+        return memo[i][j] = ans;
     }
 
     public static void main(String[] args) {
-        char mat[]= {'n','i','t','i'};
+        char mat[]= {'n','i','t','k'};
         int i=0,j=mat.length;
-        int an = solve(mat,i,j-1);
+
+        int memo[][] = new int[j+1][j+1];
+        for(int rows[]: memo)
+            Arrays.fill(rows, -1);
+        
+        int an = solve(mat,i,j-1,memo);
         System.out.print(an);
     }
 }
