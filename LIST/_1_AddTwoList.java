600cfab725de225f1d87cefd03ed7191dedfc7a9
Akshar Kashyap <aksharcashyap@gmail.com>
2020-05-28 20:50:01 +0530

    LIST/_:

--- LIST/_1_AddTwoList.java
+++ LIST/_1_AddTwoList.java
@@ -11,56 +11,50 @@ Input: List1: 7->5->9->4->6  // represents number 64957
        List2: 8->4 //  represents number 48
 Output: Resultant list: 5->0->0->5->6  // represents number 65005
  */
-
-class AddTwoList{
+// O(m + n) where m and n are number of nodes in first and second lists respectively.
+class _1_AddTwoList{
 
     //note -> here h1,h2 are reversed list
     // to get the answer you can reverse the answer list or read it from right to left
+    // here we are dealing with the single digits every time so carry can be 1 or 0 only
+    // note that every node will contain single digit value only 
 
-
-    int generateNode(ListNode dummy, int carry, ListNode head){
-        int num = head.val + carry;
+    int generateNode(ListNode dummy, int carry, int h1, int h2){
+        int num = h1 + h2 +  carry;
         int nodeVal = num % 10;
         dummy.next = new ListNode(nodeVal);
-        carry = num/10;
-        return carry;
+        return num >= 10 ?  1 :  0; //carry  = num/10;
     }
 
     void solve(ListNode h1, ListNode h2,ListNode h3, int carry){
         ListNode dummy=h3; //you can return it as well
 
         while(h1!=null && h2!=null){    // run upto equal length
-                int num = h1.val + h2.val + carry;
-                int nodeVal = num % 10;
-                dummy.next = new ListNode(nodeVal);
-                carry = num/10;
-                h1 = h1.next;
-                h2 = h2.next;
-                dummy = dummy.next;
+            carry = generateNode(dummy, carry, h1.val,h2.val);
+            h1 = h1.next;
+            h2 = h2.next;
+            dummy = dummy.next;
             }
 
         while(h1!=null){
-            carry = generateNode(dummy, carry, h1); //check if h1 is left
+            carry = generateNode(dummy, carry, h1.val,0); //check if h1 is left
             h1 = h1.next;
             dummy = dummy.next;
         }
+
         while(h2!=null){
-            carry = generateNode(dummy, carry, h2);  //check if h2 is left
+            carry = generateNode(dummy, carry, h2.val,0);  //check if h2 is left
             h2 = h2.next;
             dummy = dummy.next;
         }
         
-        while(carry !=0){
-            int nodeVal = carry % 10;
-            dummy.next = new ListNode(nodeVal);
-            carry = carry/10;
-            dummy = dummy.next;
-        }
+        if(carry !=0) dummy.next = new ListNode(carry);
+        
     }
 
     public static void main(String[] args) {
         LinkedListUtil util = new LinkedListUtil();
-        AddTwoList list = new AddTwoList();
+        _1_AddTwoList list = new _1_AddTwoList();
 
         ListNode h1 = new ListNode(5);
         h1.next = new ListNode(4);
