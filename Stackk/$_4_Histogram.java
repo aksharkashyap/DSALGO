7f31237eb15e1bd7f19593c80dee9e2c8a63978c
Akshar Kashyap <aksharcashyap@gmail.com>
2020-05-29 21:03:15 +0530

    Stackk/$_4_Histogram: create

---
+++ Stackk/$_4_Histogram.java
@@ -0,0 +1,19 @@
+package Stackk;
+/**
+ * Find the largest rectangular area possible in a given histogram where the largest rectangle can be made of a
+ *  number of contiguous bars. For simplicity,assume that all bars have same width and the width is 1 unit.
+For example, consider the following histogram with 7 bars of heights {6, 2, 5, 4, 5, 2, 6}. The largest 
+possible rectangle possible is 12 (see the below figure, the max area rectangle is highlighted in red)
+ */
+class Histogram{
+
+}
+
+
+
+/**
+ * Brute force O(n^2)
+ * divide and conquer - avg-  O(nlogn) but in worst case O(n^2)
+ * segment tree O(nlogn)
+ * stack O(n)
+ */
