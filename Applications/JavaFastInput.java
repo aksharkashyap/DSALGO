7f5ad843a7a834af6c77936835c0e93fef06ff27
Akshar Kashyap <aksharcashyap@gmail.com>
2020-06-01 15:48:57 +0530

    Applications/Java:

---
+++ Applications/JavaFastInput.java
@@ -0,0 +1,40 @@
+package Applications;
+import java.util.*;
+import java.io.*;
+
+class JavaFastInput {
+static class FastReader{ 
+		BufferedReader br; StringTokenizer st; 
+		public FastReader(){ br = new BufferedReader(new InputStreamReader(System.in));} 
+		String next(){ 
+			while (st == null || !st.hasMoreElements()){ 
+				try{st = new StringTokenizer(br.readLine()); } 
+				catch (IOException e){e.printStackTrace();}} 
+			return st.nextToken(); 
+		} 
+		int nextInt(){return Integer.parseInt(next());} 
+		long nextLong(){return Long.parseLong(next());} 
+		double nextDouble(){return Double.parseDouble(next());} 
+		String nextLine(){ 
+			String str = ""; 
+			try{str = br.readLine();} 
+			catch (IOException e){e.printStackTrace();} 
+			return str; 
+		} 
+
+public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		FastReader s=new FastReader(); 
+		int t = s.nextInt(); 
+		while (t-- > 0) 
+		{ 	int n = s.nextInt(); 
+		    int arr[] = new int[n];
+		    for(int i=0;i<n;i++)
+			    arr[i] = s.nextInt(); 
+			//int ans = find_pair(arr,arr.length);
+			//System.out.println(ans);
+		} 
+	}
+}
+}
