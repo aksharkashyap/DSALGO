90c08fc744d76b629f2d7a066020073d9114d59c
Akshar Kashyap <aksharcashyap@gmail.com>
2020-06-09 20:07:34 +0530

    LIST/_4_PalindromeLinkedList:

--- LIST/_4_PalindromeLinkedList.java
+++ LIST/_4_PalindromeLinkedList.java
@@ -1,6 +1,10 @@
 package LIST;
 import LIST.LinkedListUtil.ListNode;
-
+/*approaches:
+1) goto middle and reverse the half list 
+2) use stack
+3) use recursion
+*/
 class PalindromeLinkedList {
     
     boolean flag=true;
@@ -16,5 +20,42 @@ class PalindromeLinkedList {
         p1=p2=head;
         solve(p1,p2);
         return flag;
+    }
+}
+
+//iterative solution
+// O(1) spcae
+// o(n) time
+class PalindromeLinkedList2 {
+    
+    public boolean isPalindrome(ListNode head) {
+        ListNode p1,mid,fast,midTemp=null;
+        mid=fast=p1=head;
+        
+        //goto midddle
+        while(fast !=null && fast.next!=null){
+            mid = mid.next;
+            fast = fast.next.next;
+        }
+        
+        ListNode t=null;
+        //start reversing
+        while(mid !=null){
+            t = mid.next;
+            mid.next = midTemp;
+            midTemp = mid;
+            mid = t; 
+        }
+        
+       //now traverse both the heads simultaneously 
+        while(midTemp!=null){
+            int x = midTemp.val;
+            int y = p1.val;
+            if(x!=y) return false;
+            midTemp=midTemp.next;
+            p1=p1.next;
+        }
+        
+        return true;
     }
 }
