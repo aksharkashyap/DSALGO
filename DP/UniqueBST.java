bd47e10faa3cc27ae36544a87e8c9b4baeec9602
Akshar Kashyap <aksharcashyap@gmail.com>
2020-06-17 16:52:48 +0530

    DP/UniqueBST:

--- DP/UniqueBST.java
+++ DP/UniqueBST.java
@@ -42,5 +42,14 @@ public class UniqueBST {
 https://www.youtube.com/watch?v=J0heuWnOKcw
 
 4) polygon triangulation
-5) hanshake across the table 
+5) hanshake across the table
+6) number of ways to form a "mountain range" with n upstrokes and n downstrokes that all stay above a horizontal line
+7) Cn−1 is the number of possible separation sequences which can separate a mixture of n components
+8) Cn is the number of ways to tile a stairstep shape of height n with n rectangles
+9) Cn is the number of noncrossing partitions of the set
+10) A convex polygon with n + 2 sides can be cut into triangles by connecting vertices with non-crossing line segments (a form of polygon triangulation). 
+The number of triangles formed is n and the number of different ways that this can be achieved is Cn
+11) Cn is the number of non-isomorphic ordered trees with n + 1 vertices
+12) Cn is the number of monotonic lattice paths along the edges of a grid with n × n square cells, which do not pass above the diagonal.
+13) Cn is the number of Dyck words of length 2n
  */
