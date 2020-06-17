97376b02d6a831cea45f9268f5d543a9f4904d6b
Akshar Kashyap <aksharcashyap@gmail.com>
2020-06-17 16:53:32 +0530

    DP/UniqueBST_Catalan: move

--- DP/UniqueBST_Catalan.java
+++ DP/UniqueBST_Catalan.java
@@ -2,7 +2,7 @@ package DP;
 //nth catlan number 
 //for amazing applications : https://en.wikipedia.org/wiki/Catalan_number
 // formula ->   2nCn/ n+1
-public class UniqueBST {
+public class UniqueBST_Catalan {
     //Recursive
     int solve_recursive(int n){
         int ans=0;
