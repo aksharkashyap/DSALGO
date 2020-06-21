b5594954510d1d170267bb687fa7005b48fc6013
Akshar Kashyap <aksharcashyap@gmail.com>
2020-06-21 11:56:57 +0530

    game

--- DP/StoneGame.java
+++ DP/StoneGame.java
@@ -49,3 +49,54 @@ class StoneGameMinMax {
         return solve(piles,0,piles.length-1,0) >=0 ;
     }
 }
+
+class Other{
+// si -> Starting Index, ei -> Ending Index
+// turn -> If True then Alex's turn else Lee's turn
+public static int gameOnInt(int[] piles, int si, int ei, boolean turn){ 
+
+    if(si > ei){
+         return 0;
+     }
+
+if(turn){ // If its Alex's Turn
+    int rr1 = gameOnInt(piles, si + 1,ei,false) + piles[si]; //Consuming from Starting Index
+    int rr2 = gameOnInt(piles, si,ei - 1,false) + piles[ei]; //Consuming form Ending Index
+    return Math.max(rr1, rr2); //Choosing the Maximum From both of the results
+}
+
+// If it is Lee's Turn
+int rr1 = gameOnInt(piles, si + 1, ei, true) - piles[si]; //Consuming from the Starting Index
+int rr2 = gameOnInt(piles, si, ei - 1, true) - piles[ei];//Consuming from the Ending Index
+return Math.min(rr1, rr2); // Choosing the Minimum from the Recursion Result
+}
+}
+
+class SolutionMemo2D {
+    int memo[][];
+    public static int gameOnIntTD(int[] piles, int si, int ei, boolean turn, int[][] strg){
+       if(si > ei){
+           return 0;
+       }
+       // If the Recursion has Calculated the Answer  
+       if(strg[si][ei] > 0){
+           return strg[si][ei]; // return that Stored Answer
+       }
+       if(turn){ // If it is Alex's Turn
+           int rr1 = gameOnIntTD(piles, si + 1,ei,false, strg) + piles[si]; // Same as Above
+           int rr2 = gameOnIntTD(piles, si,ei - 1,false, strg) + piles[ei]; // Same as Above
+           strg[si][ei] = Math.max(rr1, rr2); // Storing the max ans at particular indices
+           return strg[si][ei];
+       }
+       // If it is Lee's Turn
+       int rr1 = gameOnIntTD(piles, si + 1, ei, true, strg) - piles[si]; //Same logic as above
+       int rr2 = gameOnIntTD(piles, si, ei - 1, true, strg) - piles[ei];//Same logic as above
+       strg[si][ei] = Math.min(rr1, rr2); //Storing the answer at particular indices
+       return strg[si][ei];
+   }
+       
+       public boolean stoneGame(int[] piles) {
+           memo = new int[piles.length+1][piles.length+1];
+           return gameOnIntTD(piles,0,piles.length-1,true,memo)>=0;
+       }
+   }
