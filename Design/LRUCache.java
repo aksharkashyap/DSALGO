6b3a9aece512067e1f21601c03f38c770ad05398
Akshar Kashyap <aksharcashyap@gmail.com>
2020-06-24 19:06:25 +0530

    Design/LRUCache:

--- Design/LRUCache.java
+++ Design/LRUCache.java
@@ -136,3 +136,139 @@ class LRUCache {
         }
     }
 }
+
+//3
+
+class Node { 
+    int key,value; 
+    Node pre,next; 
+    public Node(int key, int value){ 
+        this.key = key; 
+        this.value = value; 
+    } 
+} 
+
+class LRUCache3 {
+    
+    private HashMap<Integer, Node> map; 
+    private int capicity,count; 
+    private Node head, tail;
+    
+    public LRUCache3(int capacity) {
+        this.capicity = capacity; 
+        map = new HashMap<>(); 
+        head = new Node(0, 0); 
+        tail = new Node(0, 0); 
+        head.next = tail; 
+        tail.pre = head; 
+        head.pre = null; 
+        tail.next = null; 
+        count=0;
+    }
+    
+    // This method works in O(1) 
+    public int get(int key){ 
+        if(map.get(key) == null) return -1; 
+        Node node = map.get(key); 
+        int result = node.value; 
+        deleteNode(node); 
+        addToHead(node); 
+        return result; 
+    }
+    
+    public void put(int key, int value) {
+        if (map.get(key) != null) { 
+            Node node = map.get(key); 
+            node.value = value; 
+            deleteNode(node); 
+            addToHead(node); 
+        } 
+        else { 
+            Node node = new Node(key, value); 
+            map.put(key, node); 
+            if (count < capicity){
+                count++;
+                addToHead(node); 
+            } 
+            else { 
+                map.remove(tail.pre.key); 
+                deleteNode(tail.pre); 
+                addToHead(node); 
+            } 
+        } 
+    }
+  
+    public void deleteNode(Node node){ 
+        node.pre.next = node.next; 
+        node.next.pre = node.pre; 
+    } 
+  
+    public void addToHead(Node node){ 
+        node.next = head.next; 
+        node.next.pre = node; 
+        node.pre = head; 
+        head.next = node; 
+    } 
+ 
+  }
+
+
+  //4
+class LRUCache4 { 
+
+	Set<Integer> cache; 
+	int capacity; 
+
+	public LRUCache4(int capacity) 
+	{ 
+		this.cache = new LinkedHashSet<Integer>(capacity); 
+		this.capacity = capacity; 
+	} 
+
+	// This function returns false if key is not 
+	// present in cache. Else it moves the key to 
+	// front by first removing it and then adding 
+	// it, and returns true. 
+	public boolean get(int key) 
+	{ 
+		if (!cache.contains(key)) 
+			return false; 
+		cache.remove(key); 
+		cache.add(key); 
+		return true; 
+	} 
+
+	/* Refers key x with in the LRU cache */
+	public void refer(int key) 
+	{		 
+		if (get(key) == false) 
+		put(key); 
+	} 
+
+	// display contents of cache 
+	public void display() 
+	{ 
+		Iterator<Integer> itr = cache.iterator(); 
+		while (itr.hasNext()) { 
+			System.out.print(itr.next() + " "); 
+		} 
+	} 
+	
+	public void put(int key) 
+	{ 
+		// If already present, then 
+		// remove it first. Note that 
+		// we are going to add later 
+		if (cache.contains(key)) 
+			cache.remove(key); 
+
+		// If cache size is full, remove the least 
+		// recently used. 
+		else if (cache.size() == capacity) { 
+			int firstKey = cache.iterator().next(); 
+			cache.remove(firstKey); 
+		} 
+
+		cache.add(key); 
+	} 
+} 
