03bf99b171012575ba6636c6c1b5f7169f92b1a3
Akshar Kashyap <aksharcashyap@gmail.com>
2020-07-20 22:44:17 +0530

    BinarySearchProblems/TerniarySearch: create

---
+++ BinarySearchProblems/TerniarySearch.java
@@ -0,0 +1,24 @@
+package BinarySearchProblems;
+
+class TerniarySearch{
+
+    static int search(int start, int end, int arr[], int key){
+
+        while(start<=end){
+            int mid1 = start + (end-start)/2;
+            int mid2 = end + (end-start)/2;
+            if(arr[mid1] == key) return mid1;
+            if(arr[mid2] == key) return mid2;
+            
+            if(key < arr[mid1]) end = mid1-1; // key lies b/w [start to mid1]
+            else if(key > arr[mid2])  start = mid2+1; //key lies b/w [mid2 to end]
+            else{ //key lies b/w [mid1 to mid2]
+                start = mid1+1;
+                end = mid2-1;
+            }
+        }
+
+        return -1;
+    }
+
+}
