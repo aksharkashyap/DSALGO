8596874d69dc47549102793b1a4bd503883a36f6
Akshar Kashyap <aksharcashyap@gmail.com>
2020-07-27 23:53:13 +0530

    cmt

--- SORTING/RadixSort.java
+++ SORTING/RadixSort.java
@@ -6,28 +6,23 @@ public class RadixSort {
 
     static int getMax(int arr[], int n){
         int max=0;
-        for(int i=0;i<n;i++)
-            max = Math.max(max, arr[i]);
+        for(int i=0;i<n;i++) max = Math.max(max, arr[i]);
         return max;
     }
     static void count_sort(int arr[],int n, int exp){
       int output[] = new int[n];
       int count[] = new int[10];
-      Arrays.fill(count,0);
 
-      for(int i=0;i<n;i++)
-        count[(arr[i]/exp)%10]++; // store in the count
+      for(int i=0;i<n;i++) count[(arr[i]/exp)%10]++; // store in the count
       
-      for(int i=1;i<10;i++)
-        count[i]  += count[i-1];  
+      for(int i=1;i<10;i++) count[i]  += count[i-1];  
       
       for(int i=n-1;i>=0;i--){ // build the output array
         output[count[(arr[i]/exp) %10]-1] = arr[i];
         count[(arr[i]/exp) %10]--;
       }
      
-      for(int i=0;i<n;i++)
-            arr[i]=output[i];
+      for(int i=0;i<n;i++) arr[i]=output[i];
     }
 
     static void radix_sort(int arr[],int n){
@@ -43,3 +38,34 @@ public class RadixSort {
         System.out.println(Arrays.toString(arr));
     }
 }
+
+
+
+/**
+ class Test{
+
+   static void countSort(int[] arr, int k){
+
+         int[] count = new int[10];
+         int output[] = new int[arr.length];
+
+         for(int i=0;i<arr.length;i++) count[(arr[i]/k) % 10]++;
+         
+         for(int i=1;i<10;i++) count[i] += count[i-1]; //prefix sum
+
+         for(int i=arr.length-1;i>=0;i--) output[--count[(arr[i]/k) % 10]] = arr[i];
+
+         int i=0; for(int v : output) arr[i++] = v;
+   }
+
+    public static void main(String[] args) {
+      
+      int arr[] = {1234,2134,9853,245,1322};
+      int max = 9853; //max element
+      for(int i=1; (max/i) >0; i=i*10){ // we need to do -> div by k mod 10 to extract digits 
+           countSort(arr, i);
+      }
+      System.out.print(Arrays.toString(arr));
+    }
+}
+ */
--- Test.java
+++ Test.java
@@ -1,6 +1,28 @@
+import java.util.Arrays;
 class Test{
+
+   static void countSort(int[] arr, int k){
+
+         int[] count = new int[10];
+         int output[] = new int[arr.length];
+
+         for(int i=0;i<arr.length;i++) count[(arr[i]/k) % 10]++;
+         
+         for(int i=1;i<10;i++) count[i] += count[i-1]; //prefix sum
+
+         for(int i=arr.length-1;i>=0;i--) output[--count[(arr[i]/k) % 10]] = arr[i];
+
+         int i=0; for(int v : output) arr[i++] = v;
+   }
+
     public static void main(String[] args) {
-        
+      
+      int arr[] = {1234,2134,9853,245,1322};
+      int max = 9853; //max element
+      for(int i=1; (max/i) >0; i=i*10){ // we need to do -> div by k mod 10 to extract digits 
+           countSort(arr, i);
+      }
+      System.out.print(Arrays.toString(arr));
     }
 }
 
