a1e52998b906fc368dc90991e32fc7994951bdb7
Akshar Kashyap <aksharcashyap@gmail.com>
2020-07-28 19:15:38 +0530

    cmt

--- Test.java
+++ Test.java
@@ -1,30 +1,8 @@
 import java.util.*;
 
 class Test{
-  
-   static void bucketSort(double[] arr, int size){
-      List<List<Double>> bucket = new ArrayList<>();
-      size = (int) Math.sqrt(size);
-
-      for(int i=0;i<size;i++) bucket.add(new ArrayList<>());
-
-      for(int i=0;i<arr.length;i++) bucket.get((int)(arr[i] * size)).add(arr[i]);
-
-      for(int i=0;i<size;i++) Collections.sort(bucket.get(i));
-
-
-      System.out.println(bucket);
-
-      int k=0; for(List<Double> v : bucket){
-         for(double val : v) arr[k++] = val;
-      }
-   }
-
     public static void main(String[] args) {
-      
-      double arr[] = {0.5,0.1,0.5,0.2,0.42,0.41,0.53};
-      bucketSort(arr,arr.length);
-      System.out.print(Arrays.toString(arr));
+     
     }
 }
 
---
+++ _CP/PrimeFactorization.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+class PrimeFactorization{
+
+    static boolean[] isPrime; // to hold all possible primes upto given value
+    static ArrayList<Integer> primes; //to hold all prime numbers upto given value
+ 
+    static void fill_prime(int val){
+       isPrime = new boolean[val+1];
+       Arrays.fill(isPrime,true); // initialize with true 
+       isPrime[0] = isPrime[1] = false;
+       for(int i=2;i<=Math.sqrt(val);i++){
+          if(!isPrime[i]) continue;
+          for(int j=i; i*j<=val; j++) isPrime[i*j] = false; 
+       }
+    }
+ 
+    static void pf(int n, int start){
+       for(int p = start; p<primes.size();p++){
+             int prime = primes.get(p);
+             if(n % prime == 0){
+                System.out.print(prime + " ");
+                pf(n/prime,p);
+                return;
+             }
+       }
+    }
+ 
+     public static void main(String[] args) {
+       int num = 353463;
+       fill_prime(num);
+       primes = new ArrayList<>();
+       for(int i=2;i<isPrime.length;i++) if(isPrime[i]) primes.add(i);
+       pf(num,0);
+     }
+ }
