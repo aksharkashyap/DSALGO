039f14c376c244a781ac1bad6f970d03b68c591f
Akshar Kashyap <aksharcashyap@gmail.com>
2020-07-28 09:50:23 +0530

    cmt

--- SORTING/BucketSort.java
+++ SORTING/BucketSort.java
@@ -77,3 +77,37 @@ bucketSortPositive(arr[], n)
 //we can use bucket sort for integers but it doesnt make any sense because the bucket size will be 1
 //and also it will take lot of space, 
 // we have better algo for integers (count sort)
+
+
+/**
+ * Complexity
+ * 
+Worst Case Complexity: O(n2)
+
+When there are elements of close range in the array, they are likely to be placed in the same bucket. 
+This may result in some buckets having more number of elements than others.
+It makes the complexity depend on the sorting algorithm used to sort the elements of the bucket.
+The complexity becomes even worse when the elements are in reverse order. 
+If insertion sort is used to sort elements of the bucket, then the time complexity becomes O(n2).
+
+Best Case Complexity: O(n+k)
+It occurs when the elements are uniformly distributed in the buckets with a nearly equal number of elements 
+in each bucket.
+The complexity becomes even better if the elements inside the buckets are already sorted.
+If insertion sort is used to sort elements of a bucket then the overall complexity in the best case will be 
+linear ie. O(n+k). 
+O(n) is the complexity for making the buckets and O(k) is the complexity for sorting the elements of 
+the bucket using algorithms having linear time complexity at the best case.
+
+
+Average Case Complexity: O(n)
+It occurs when the elements are distributed randomly in the array. 
+Even if the elements are not distributed uniformly, bucket sort runs in linear time. 
+It holds true until the sum of the squares of the bucket sizes is linear in the total number of elements.
+
+
+Bucket Sort Applications
+    Bucket sort is used when:
+    input is uniformly distributed over a range.
+    there are floating point values
+ */
