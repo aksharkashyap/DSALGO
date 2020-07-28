da93d399702cbe341a4799104097c76c00f413f6
Akshar Kashyap <aksharcashyap@gmail.com>
2020-07-28 19:56:37 +0530

    Test:

--- Test.java
+++ Test.java
@@ -1,9 +1,26 @@
 import java.util.*;
 
 class Test{
-    public static void main(String[] args) {
-     
+
+    int getFactor(int num, int p){
+        if(num % p != 0) return num;
+        System.out.print(p + " "); 
+        return getFactor(num/p, p);
     }
+
+    int go(int num, int start, int end){
+        if(start > end) return;
+        num = getFactor(num,start);
+        return go(num,start+2,end);
+    }
+
+    public static void primeFactors(int n){ 
+        getFactor(num,2); 
+        int n = go(n,3,Math.sqrt(n));
+        if (n > 2) System.out.print(n); 
+    } 
+
+    
 }
 
 /**
