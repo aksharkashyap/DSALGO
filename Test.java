ce5daff0dc8f8cd27d08fb684983ec5fec6e3949
Akshar Kashyap <aksharcashyap@gmail.com>
2020-07-30 08:30:21 +0530

    Test:

--- Test.java
+++ Test.java
@@ -1,42 +1,46 @@
 import java.util.*;
 
 class Test{
-        static int primes[];
-        static void simpleSeive(int LIMIT){//calculate primes till root(R)
-            boolean[] bool_prime = new boolean[LIMIT+1]; int size=0;
-            Arrays.fill(bool_prime,true);
-            for(int i=2; i<=LIMIT;i++){
-                if(!bool_prime[i]) continue;
-                for(int j = i*i; j<=LIMIT; j+=i) bool_prime[j] = false;
-                size++;
-            }
-            //store the prime numbers in integer form
-            primes = new int[size];
-            int k=0; primes[k++] = 2;
-            for(int i=3;i<bool_prime.length;i+=2) if(bool_prime[i]) primes[k++]=i;
-    
-        }
-    
-        static void calculate(int L, int R){
-            int n = R-L+1;
-            boolean[] primeInRange = new boolean[n+1];
-        
-            for(int i=0;i<primes.length;i++){
-                int base = (L / primes[i]) * primes[i];
-                if(base < L || base == primes[i]) base += primes[i];
-                for(int j=base;j<=R;j+=primes[i]) 
-                    primeInRange[j-L] = true; // true indicates these nums are not prime
-            }
-            //print the primes in the given range
-            for(int i=L;i<=R;i++)
-                if(!primeInRange[i-L]) System.out.print(i+" ");
-        }
-    
-        public static void main(String[] args) {
-            int L = 12, R = 50;
-            simpleSeive((int)Math.sqrt(R));
-            calculate(L,R);
-        }
+   class Solution {
+      public int ladderLength(String bW, String eW, List<String> wordList) {
+          
+          int count=0;
+          int j=0;
+          while(j++<5){
+              int count_bW[] = new int[26];
+              for(int i=0;i<bW.length();i++) count_bW[bW.charAt(i)-97]++;
+              
+              for(String word : wordList){
+                  int count_word[] = new int[26];
+                  for(int i=0;i<word.length();i++) count_word[word.charAt(i)-97]++;
+                  int diff=0;
+                 
+                  for(int i=0;i<26;i++){
+                      
+                      if(count_word[i] != count_bW[i]) diff++;
+                      if(diff>1) break;
+                  }
+                  if(diff==1){
+                       
+                      bW = word;
+                      count++;
+                      break;
+                  }
+              }
+          }
+          
+          return count;
+      }
+  }
+  
+  /*
+  for hit find the word which differ in the frequency only at one place
+  hot (is it equal to endWord -> cont)
+  dot (is it equal to endWord -> cont)
+  dog (is it equal to endWord -> cont)
+  cog (is it equal to endWord -> stop)
+      
+  */
 }
 
 /**
