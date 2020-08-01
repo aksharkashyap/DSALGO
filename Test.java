b1767a61d38704baacd765dc09fe8291e5b87c7d
Akshar Kashyap <aksharcashyap@gmail.com>
2020-08-01 23:13:09 +0530

    cmt

--- Test.java
+++ Test.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
 //Prison cell after n days
-class Test {
+class PrisonCell {
     //brute force
     public int[] prisonAfterNDays(int[] cells, int N) {
         int len = cells.length;
@@ -14,6 +14,31 @@ class Test {
                 else temp[i] = 0;
             }
             cells = Arrays.copyOf(temp,temp.length);
+        }
+        return cells;
+    }
+}
+
+
+class PrisonCell2 {
+    public int[] prisonAfterNDays(int[] cells, int N){
+        int len = cells.length;
+        int temp[] = new int[cells.length];
+        Set<String> seen = new HashSet<>();
+        int cycleLen=0;
+        for(int n=0;n<N;n++){
+            Arrays.fill(temp,0);
+            for(int i=1;i<len-1;i++){
+                if(cells[i-1]==0 && cells[i+1] == 0 ||
+                   cells[i-1]==1 && cells[i+1] == 1 ) 
+                    temp[i] = 1;
+                else temp[i] = 0;
+            }
+            if(!seen.add(Arrays.toString(temp))){
+                return prisonAfterNDays(cells,N % cycleLen);
+            }
+            cells = Arrays.copyOf(temp,temp.length);
+            cycleLen++;
         }
         return cells;
     }
