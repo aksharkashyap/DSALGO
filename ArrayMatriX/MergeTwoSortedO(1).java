142fb94d0375c3a339dde30ed9f8de2c4cab0c6f
Akshar Kashyap <aksharcashyap@gmail.com>
2020-08-02 22:19:04 +0530

    cmt

---
+++ ArrayMatriX/MergeTwoSortedO(1).java
@@ -0,0 +1,44 @@
+package ArrayMatrix;
+import java.util.*;
+
+//insertion sort based
+class MergeTwoSorted{
+    /** time O(m*n) [linear * insertionSort]
+     * 
+     */
+   
+    static void swap(int arr[], int i, int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    static void insertionSort(int arr[]){
+        for(int i=1;i<arr.length;i++){
+            if(arr[i] < arr[i-1]) swap(arr,i-1,i);
+        }
+    }
+
+    static void merge(int a[], int b[], int alen, int blen){
+
+        for(int i=0;i<alen;i++){
+            if(b[0] < a[i]){
+                int temp = a[i];
+                a[i] = b[0];
+                b[0] = temp;
+                insertionSort(b);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int b[] = {44,48,77,88};
+        int a[] = {1,32,46,47,78,80};
+        int alen = a.length;
+        int blen = b.length;
+        merge(a,b,alen,blen-1);
+        System.out.print(Arrays.toString(a) + " " + Arrays.toString(b));
+    }
+}
+
+//GAP based
--- Test.java
+++ Test.java
@@ -1,46 +1,13 @@
 import java.util.*;
 
-//Prison cell after n days
-class PrisonCell {
-    //brute force
-    public int[] prisonAfterNDays(int[] cells, int N) {
-        int len = cells.length;
-        int temp[] = new int[cells.length];
-        
-        while(N-- > 0){
-            Arrays.fill(temp,0);
-            for(int i=1;i<len-1;i++){
-                if(cells[i-1]==0 && cells[i+1] == 0 || cells[i-1]==1 && cells[i+1] == 1 ) temp[i] = 1;
-                else temp[i] = 0;
-            }
-            cells = Arrays.copyOf(temp,temp.length);
-        }
-        return cells;
-    }
-}
-
-
-class PrisonCell2 {
-    public int[] prisonAfterNDays(int[] cells, int N){
-        int len = cells.length;
-        int temp[] = new int[cells.length];
-        Set<String> seen = new HashSet<>();
-        int cycleLen=0;
-        for(int n=0;n<N;n++){
-            Arrays.fill(temp,0);
-            for(int i=1;i<len-1;i++){
-                if(cells[i-1]==0 && cells[i+1] == 0 ||
-                   cells[i-1]==1 && cells[i+1] == 1 ) 
-                    temp[i] = 1;
-                else temp[i] = 0;
-            }
-            if(!seen.add(Arrays.toString(temp))){
-                return prisonAfterNDays(cells,N % cycleLen);
-            }
-            cells = Arrays.copyOf(temp,temp.length);
-            cycleLen++;
-        }
-        return cells;
+class Test{
+    public static void main(String[] args) {
+        int b[] = {44,48,77,88};
+        int a[] = {1,32,46,47,78,80};
+        int alen = a.length;
+        int blen = b.length;
+        merge(a,b,alen,blen-1);
+        System.out.print(Arrays.toString(a) + " " + Arrays.toString(b));
     }
 }
 
