ee3b6d800e6ff76f58fc292ca539d401a2aad1a5
Akshar Kashyap <aksharcashyap@gmail.com>
2020-08-13 15:04:31 +0530

    cmt

--- Test.java
+++ Test.java
@@ -2,7 +2,9 @@ import java.util.*;
 
 class Test {
     public static void main(String[] args){
-       
+       int x = 4798797;
+       int digits = (int) Math.floor(Math.log10(x) +1);
+       System.out.print(digits);
     }
 }
 /**
