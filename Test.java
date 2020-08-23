b7fd642662a6d9c078f9cc3a6730b348f84d8af0
Akshar Kashyap <aksharcashyap@gmail.com>
2020-08-23 17:06:03 +0530

    cmt

--- Test.java
+++ Test.java
@@ -1,28 +1,7 @@
-
 class Test{
 
 	public static void main(String[] args) {
-		String str = "abcabcabcabcabccc";
-		char[] arr = str.toCharArray();
-		int max=0;
-		StringBuilder ans = new StringBuilder("");
-		for(int i=0;i<arr.length;i++){
-			StringBuilder sb = new StringBuilder("");
-			int count[] = new int[26];
-			int len=0;
-			for(int j=i;j<arr.length;j++){
-				count[arr[j]-97]++;
-				if(count[arr[j]-97] == 1) len++;
-				sb.append(arr[j]);
-				if(len==2){
-					if(j-i>=max){
-						ans = sb;
-						max = j-i;
-					}
-				}
-				else if(len>2) break;
-			}
-		}
-		System.out.println(ans.toString());
+		int x = '9' -'0';
+		System.out.println(x);
 	}
 }
