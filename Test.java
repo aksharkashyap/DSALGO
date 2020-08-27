fd2f0380735526ae28d0696892aedd5294320ed4
Akshar Kashyap <aksharcashyap@gmail.com>
2020-08-27 21:08:52 +0530

    REM

--- Test.java
+++ Test.java
@@ -7,9 +7,8 @@ class Test{
 }
 
 /*
-https://leetcode.com/problems/verifying-an-alien-dictionary/
-https://leetcode.com/problems/decode-ways/
 https://leetcode.com/problems/regular-expression-matching/
+https://leetcode.com/problems/flatten-binary-tree-to-linked-list/
 https://leetcode.com/problems/remove-invalid-parentheses/
 https://leetcode.com/problems/next-permutation/
 https://leetcode.com/problems/accounts-merge/
@@ -19,14 +18,12 @@ https://leetcode.com/problems/friends-of-appropriate-ages/
 https://leetcode.com/problems/multiply-strings/
 https://leetcode.com/problems/palindrome-pairs/
 https://leetcode.com/problems/longest-valid-parentheses/
-https://leetcode.com/problems/flatten-binary-tree-to-linked-list/
 https://leetcode.com/problems/permutation-in-string/
 https://leetcode.com/problems/frog-jump/
 https://leetcode.com/problems/interval-list-intersections/
 https://leetcode.com/problems/n-queens/
 https://leetcode.com/problems/insert-interval/
 https://leetcode.com/problems/recover-binary-search-tree/
-https://leetcode.com/problems/sliding-window-maximum/
 https://leetcode.com/problems/shortest-subarray-with-sum-at-least-k/
 https://leetcode.com/problems/longest-consecutive-sequence/
 https://leetcode.com/problems/roman-to-integer/
@@ -37,9 +34,9 @@ https://leetcode.com/problems/check-completeness-of-a-binary-tree/
 https://leetcode.com/problems/battleships-in-a-board/
 https://leetcode.com/problems/add-two-numbers/
 https://leetcode.com/problems/remove-zero-sum-consecutive-nodes-from-linked-list/
-https://leetcode.com/problems/convert-sorted-list-to-binary-search-tree/
 https://leetcode.com/problems/split-linked-list-in-parts/
 https://leetcode.com/problems/unique-paths-ii/
-https://leetcode.com/problems/increasing-triplet-subsequence/
 https://leetcode.com/problems/longest-substring-with-at-least-k-repeating-characters/
+https://leetcode.com/problems/convert-sorted-list-to-binary-search-tree/
+https://www.geeksforgeeks.org/count-inversions-array-set-3-using-bit/
 */
