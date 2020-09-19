30a927c0ea73ddd28ff3827fd811ca64b6190789
Akshar Kashyap <aksharcashyap@gmail.com>
2020-09-19 22:41:23 +0530

    cmt

--- Test.java
+++ Test.java
@@ -1,11 +1,23 @@
 import java.util.*;
 
+class Student{
+	int a;
+	String b;
+	Student(int a, String b){
+		this.a=a;
+		this.b=b;
+	}
+	@Override
+	public String toString(){
+		return a +" "+b;
+	}
+}
+
 class Test {
-
 	public static void main(String[] args){
-
-		System.out.println(Arrays.toString("barfoothefoobarman".split("(?<=\\G.{3})")));
-
+		List<Student> v = new ArrayList<>();
+		Student s = new Student(1,"hi");
+		System.out.print(s);
 	}
 	static int val(char ch){return ch-48;}
 }
