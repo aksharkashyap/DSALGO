8e5ca6fd22f5ade5c8278900240064907f5c1823
Akshar Kashyap <aksharcashyap@gmail.com>
2020-10-22 22:12:07 +0530

    cmt

--- Test.java
+++ Test.java
@@ -1,69 +1,22 @@
 import java.util.*;
-import java.lang.*;
-import java.io.*;
 
-class Atm{
-    public static void main(String args[])
-    {
-        FastReader sc = new FastReader();
-        int T=sc.nextInt();
-        for(int a=1;a<=T;a++)
-        {
-            int N = sc.nextInt();
-            int X = sc.nextInt();
-            int arr[]=new int[N];
-            for(int i=0;i<N;i++)
-            {
-                arr[i]=sc.nextInt();
-            }
-            Queue<Integer> q1=new LinkedList<>();
-            Queue<Integer> q2=new LinkedList<>();
-            List<Integer> ans=new ArrayList<>();
-            for(int i=1;i<=N;i++)
-            {
-                q1.add(i);
-                q2.add(arr[i-1]);
-            }
-            while(!q1.isEmpty())
-            {
-                int temp1=q1.poll();
-                int temp2=q2.poll();
-                if(temp2<=X)
-                {
-                    ans.add(temp1);
-                }
-                else
-                {
-                    q1.add(temp1);
-                    q2.add(temp2-X);
-                }
-            }
-            System.out.print("Case #"+a+": ");
-            for(int i=0;i<ans.size();i++)
-            {
-                System.out.print(ans.get(i)+" ");
-            }
-        }
+class Test{
+
+    static int solve(int arr[], int lo, int hi){
         
-    }
+        if(lo == hi) return lo;
+        if(hi-lo == 1) return arr[lo] > arr[hi] ? lo : hi;
     
-    static class FastReader{ 
-		BufferedReader br; StringTokenizer st; 
-		public FastReader(){ br = new BufferedReader(new InputStreamReader(System.in));} 
-		String next(){ 
-			while (st == null || !st.hasMoreElements()){ 
-				try{st = new StringTokenizer(br.readLine()); } 
-				catch (IOException e){e.printStackTrace();}} 
-			return st.nextToken(); 
-		} 
-		int nextInt(){return Integer.parseInt(next());} 
-		long nextLong(){return Long.parseLong(next());} 
-		double nextDouble(){return Double.parseDouble(next());} 
-		String nextLine(){ 
-			String str = ""; 
-			try{str = br.readLine();} 
-			catch (IOException e){e.printStackTrace();} 
-			return str; 
-		} 
-	}
+        int mid = lo + (hi-lo)/2;
+        int left = solve(arr, lo, mid-1);
+        int right = solve(arr,mid,hi);
+
+        return arr[left] > arr[right] ? left : right;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1,2,39,10,4,11,2,44};
+        int ans = solve(arr,0,arr.length-1);
+        System.out.print(arr[ans]);
+    }
 }
