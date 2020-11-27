89d89e924df5194ca348b098cd64e8881613d614
Akshar Kashyap <aksharcashyap@gmail.com>
2020-11-27 10:54:13 +0530

    sum treee

---
+++ TREE/BinaryTreeToSumTree.java
@@ -0,0 +1,40 @@
+package TREE;
+/*
+Given a Binary Tree where each node has positive and negative values.
+Convert this to a tree where each node contains the sum of the left and right sub trees in the original tree.
+The values of leaf nodes are changed to 0.
+
+For example, the following tree
+
+                  10
+               /      \
+             -2        6
+           /   \      /  \ 
+         8     -4    7    5
+        
+         should be changed to
+
+                 20(4-2+12+6)
+               /      \
+         4(8-4)      12(7+5)
+           /   \      /  \ 
+         0      0    0    0
+
+*/
+class Tree{
+    public void toSumTree(Node root){
+         //add code here.
+         solve(root);
+    }
+    
+    int solve(Node root){
+        if(root == null) return 0;
+        int originalLeft = solve(root.left); //to store original(given)value of left child
+        int originalRight = solve(root.right); //to store original value of right child
+        int root_val = root.data;
+        int updatedLeft = (root.left == null ? 0 : root.left.data);
+        int updatedRight = (root.right == null ? 0 : root.right.data);
+        root.data = updatedLeft + updatedRight + originalLeft + originalRight; //update current root
+        return root_val;  // send root's previous value for parent
+    }
+}
