fb5d51632dc7b509367541b2b302133703915553
Akshar Kashyap <aksharcashyap@gmail.com>
2020-12-11 22:22:28 +0530

    updated cp template

--- _CP_Template/CodeChef_test.java
+++ _CP_Template/CodeChef_test.java
@@ -83,6 +83,21 @@ class CodeChef_test
 			}
 			return neg*integer;
 		}
+		public long nextLong()throws IOException{
+			long ll=0l;
+			int n=scan();
+			while(isWhiteSpace(n)) n=scan();
+			int neg=1;
+			if(n=='-'){ neg=-1; n=scan();}
+			while(!isWhiteSpace(n)){
+				if(n>='0'&&n<='9'){
+					ll*=10; ll+=n-'0';
+					n=scan();
+				}
+				else throw new InputMismatchException();
+			}
+			return neg*ll;
+		}
 		public double nextDouble()throws IOException{
 			double doub=0; int n=scan();
 			while(isWhiteSpace(n))
--- _CP_Template/FastInputHackerEarth.java
+++ _CP_Template/FastInputHackerEarth.java
@@ -31,6 +31,21 @@ class FastScanner{
         }
         return neg*integer;
     }
+    public long scanLong()throws IOException{
+        long ll=0l;
+        int n=scan();
+        while(isWhiteSpace(n)) n=scan();
+        int neg=1;
+        if(n=='-'){ neg=-1; n=scan();}
+        while(!isWhiteSpace(n)){
+            if(n>='0'&&n<='9'){
+                ll*=10; ll+=n-'0';
+                n=scan();
+            }
+            else throw new InputMismatchException();
+        }
+        return neg*ll;
+    }
     public double scanDouble()throws IOException{
         double doub=0; int n=scan();
         while(isWhiteSpace(n))
